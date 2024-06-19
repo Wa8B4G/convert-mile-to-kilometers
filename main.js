230a991ef41aeb97b544d0631c13e7969028c0d3
Clever <ebinumclever28@gmail.com>
2024-06-19 07:41:51 -0700

    updated commit

--- main.js
+++ main.js
@@ -7,13 +7,14 @@ document.getElementById("submit").addEventListener("click", function(e){
     const distance = document.getElementById("kilo");
     const val = distance.value;
     if (val) {
-        
+      disA.style.display = "none";
+      distance.style.border = "1px solid blue";
       displayName.innerHTML = (val * 1.60934)
         
     }else {
         // const dval = val * 10;
        
-        
+        distance.style.border = "1px solid red";
         disA.style.display = "block";
 
     }
